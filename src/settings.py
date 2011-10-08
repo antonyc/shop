47e99b099a7dacec520d5306f14d229e72742562
Chaporgin Anton <chapson@yandex-team.ru>
2011-10-08 19:34:35 +0400

    Made static files half empty

--- src/settings.py
+++ src/settings.py
@@ -31,6 +31,10 @@ DATABASES = {
     }
 }
 
+MONGO_DATABASES={'default':{'NAME': 'shop',
+                            'HOST': '127.0.0.1'
+}}
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -57,7 +61,7 @@ USE_L10N = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = '/usr/share/amadika/shop/media/'
-
+UPLOAD_PATH = 'user_uploads'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
@@ -122,13 +126,11 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
+#    'django.contrib.auth',
     'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.gis',
     'catalog',
