14ef6e3862d544f53bc2e084037f363a0f6edf21
Chaporgin Anton <chapson@yandex-team.ru>
2011-10-09 15:11:11 +0400

    No static filles

--- src/settings.py
+++ src/settings.py
@@ -93,7 +93,7 @@ STATICFILES_DIRS = (
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
-#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.staticfiles',
     'django.contrib.messages',
     'django.contrib.humanize',
     'django.contrib.gis',
