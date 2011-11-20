fc94312a3c80a47e104b79f23abd21b01aa6f158
Chaporgin Anton <chapson@yandex-team.ru>
2011-11-20 12:29:44 +0400

    Repaired block "no_second_submit"

--- src/utils/static/block/no_second_submit/no_second_submit.js
+++ src/utils/static/block/no_second_submit/no_second_submit.js
@@ -3,11 +3,15 @@ jQuery(function() {
     $('.block-no_second_submit').each(function() {
         var _this = jQuery(this);
         $('form', this).submit(function(){
-            var handler = function(){
-                var submit = $('input[type=submit]', this);
-                submit.attr('disabled', 'disabled');
-                $('.spinner', _this).show();
-            }
+            var submit = $('input[type=submit]', this);
+            submit.attr('disabled', 'disabled');
+            submit.blur();
+            var handler = function(context){
+                return function(){
+                    console.log(10);
+                    $('.block-spinner', context).show();
+                }
+            }(_this);
             setTimeout(handler, 1000)
         })
     })
