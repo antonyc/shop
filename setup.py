83858f6995858b82980c893bde715a0bdd07e0ee
Chaporgin Anton <chapson@yandex-team.ru>
2014-06-29 19:07:06 +0400

    вроде работает кое-что

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pets_app',
+    name='pets_application',
     description='Backend for pets application',
     long_description='',
     author='Chaporgin Anton',
     author_email='mrdark@list.ru',
-    packages=find_packages(''),
+    packages=find_packages(),
     package_dir={'': ''},
-    include_package_data=True,
-    zip_safe=True,
+    # include_package_data=True,
+    # zip_safe=True,
     version='0.1',
     install_requires=[
         'djangorestframework==2.3.13',
@@ -20,6 +20,7 @@ setup(
         'pymongo==2.7',
         'Django==1.6.5',
         'requests==2.3',
+        'uwsgi==2.0.5',
     ],
-    py_modules=['sitecustomize']  # added additional single module from root 'src' folder
+    # py_modules=['sitecustomize']  # added additional single module from root 'src' folder
 )
